e9c4b9973df2f7d3614cdda326054fd11fb2e2da
numan bulama <numanbulama54@gmail.com>
2024-10-02 16:14:26 +0300

    refactor package

--- src/main/java/com/example/car/CarApplication.java
+++ src/main/java/com/example/car/CarApplication.java
@@ -1,4 +1,4 @@
-package com.example.car.car;
+package com.example.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/car/CarApplicationTests.java
+++ src/test/java/com/example/car/CarApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.car.car;
+package com.example.car;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
